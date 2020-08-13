9393788815fdc4cc50147353f4ffbdaa9c6eb1bd
iXiaowu <zhangxiaotong@yahoo.com>
2020-08-13 11:27:42 +0800

    update connectors

--- src/stores/connectors.jsx
+++ src/stores/connectors.jsx
@@ -1,5 +1,5 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
-// import { NetworkConnector } from "@web3-react/network-connector";
+import { NetworkConnector } from "@web3-react/network-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { WalletLinkConnector } from "@web3-react/walletlink-connector";
 import { LedgerConnector } from "@web3-react/ledger-connector";
@@ -21,11 +21,11 @@ export const injected = new InjectedConnector({
   supportedChainIds: [1, 3, 4, 5, 42]
 });
 
-// export const network = new NetworkConnector({
-//   urls: { 1: RPC_URLS[1], 4: RPC_URLS[4] },
-//   defaultChainId: 1,
-//   pollingInterval: POLLING_INTERVAL
-// });
+export const network = new NetworkConnector({
+  urls: { 1: RPC_URLS[1], 4: RPC_URLS[4] },
+  defaultChainId: 1,
+  pollingInterval: POLLING_INTERVAL
+});
 
 export const walletconnect = new WalletConnectConnector({
   rpc: { 1: RPC_URLS[1] },
@@ -36,7 +36,7 @@ export const walletconnect = new WalletConnectConnector({
 
 export const walletlink = new WalletLinkConnector({
   url: RPC_URLS[1],
-  appName: "iearn.financaae"
+  appName: "yfii.finance"
 });
 
 export const ledger = new LedgerConnector({
@@ -49,19 +49,19 @@ export const trezor = new TrezorConnector({
   chainId: 1,
   url: RPC_URLS[1],
   pollingInterval: POLLING_INTERVAL,
-  manifestEmail: "dummy@abc.xyz",
-  manifestAppUrl: "https://8rg3h.csb.app/"
+  manifestEmail: "igaojin@qq.com",
+  manifestAppUrl: "https://yfii.finance/"
 });
 
 export const frame = new FrameConnector({ supportedChainIds: [1] });
 
 export const fortmatic = new FortmaticConnector({
-  apiKey: "pk_live_F95FEECB1BE324B5",
+  apiKey: "pk_live_BC11B495039A58C8",
   chainId: 1
 });
 
 export const portis = new PortisConnector({
-  dAppId: "211b48db-e8cc-4b68-82ad-bf781727ea9e",
+  dAppId: "dabe0712-1427-429f-b878-115a1586e9e4",
   networks: [1, 100]
 });
 
